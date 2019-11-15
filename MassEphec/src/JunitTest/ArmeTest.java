c3e94066cc014d8d0c31fc30bdd722046fa10c0d
Emmanuel <e.foureau@students.ephec.be>
2019-11-15 17:44:55 +0100

    Ajout de la classe arme + test

---
+++ MassEphec/src/JunitTest/ArmeTest.java
@@ -0,0 +1,44 @@
+/**
+ * Classe test de la classe Arme
+ */
+package JunitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import model.Arme;
+import model.Personnage;
+
+/**
+ * @author emmanuel
+ * Date de création : 15/11/19
+ */
+class ArmeTest {
+	
+
+	/**
+	 * Classe test pour la classe "setExperience"
+	 */
+	@Test
+	void testSetExperience() {
+		Arme test = new Arme();
+		test.setExperience(101);
+		assertEquals(1, test.getExperience());
+		test.setExperience(-1);
+		assertEquals(0, test.getExperience());
+		test.setExperience(1);
+		assertEquals(1, test.getExperience());
+	}
+	
+	/**
+	 * Classe test pour la classe "niveauSperieur"
+	 */
+	
+	void testAjouterExp() {
+		Arme test = new Arme();
+		test.setNiveau(1);
+		test.niveauSuperieur();
+		assertEquals(2, test.getNiveau());
+	}
+}
---
+++ MassEphec/src/model/Arme.java
@@ -0,0 +1,121 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author emman
+ *
+ */
+public class Arme {
+	// Variables d'instance 
+	private String nomArme;
+	private int experience, niveau, degat;
+	private final int experienceMax = 100;
+	
+	/**
+	 * Setter de la variable "nomArme"
+	 * @param nomArme : La nouvelle valeur de "nomArme" pour l'objet courant
+	 */
+	
+	public void setNomArme (String nomArme) {
+		this.nomArme = nomArme;
+	}
+	
+	/**
+	 * Getter de la variable "nomArme"
+	 * @return nomArme : La valeur de la variable "nomArme" de l'objet courant
+	 */
+	
+	public String getNomArme () {
+		return this.nomArme;
+	}
+	
+	/**
+	 * Setter de la variable "experience"
+	 * experience < experienceMax && experience => 0
+	 * @param experience : La nouvelle valeur d'"experience" pour l'objet courant
+	 */
+	
+	public void setExperience (int experience) {
+		if (experience >= 0 && experience < experienceMax) {
+			this.experience = experience;
+		}
+	}
+	
+	/**
+	 * Getter de la variable "experience"
+	 * @return experience : La valeur de la variable "experience" de l'objet courant
+	 */
+	
+	public int getExperience () {
+		return this.experience;
+	}
+	
+	/**
+	 * Setter de la variable "niveau"
+	 * @param niveau : La nouvelle valeur de la variable "niveau" pour l'objet courant
+	 */
+	
+	public void setNiveau (int niveau) {
+		if (niveau > 0) {
+			this.niveau = niveau;
+	}
+}
+	/**
+	 * Getter de la variable "niveau"
+	 * @return niveau : La valeur de la variable "niveau" de l'objet courant
+	 */
+	
+	public int getNiveau () {
+		return this.niveau;
+	}
+	
+	/**
+	 * Setter de la variable "degat"
+	 * @param degat : La nouvelle valeur de la variable "degat" pour l'objet courant
+	 */
+	
+	public void setDegat (int degat) {
+		if (degat >= 0) {
+			this.degat = degat;
+		}
+	}
+	/**
+	 * Getter de la variable "degat"
+	 * @return degat : La valeur de la variable "degat" de l'objet courant
+	 */
+	
+	public int getDegat () {
+		return this.degat;
+	}
+	
+	/**
+	 * Cette classe permet d'ajouter de l'exp à l'arme
+	 * Si experience >= experienceMax alors arme.niveauSuperieur()
+	 * @param experience
+	 */
+	
+	public void ajouterExp(int experience) {
+		if ((this.experience + experience) >= experienceMax) {
+			this.setExperience(this.experience + experience - experienceMax);
+			this.niveauSuperieur();
+		}
+		else if (this.experience >= 0){
+			this.setExperience(this.experience + experience);
+		}
+	}
+	
+	/**
+	 * Cette classe permet l'augmentation du niveau 
+	 * de l'arme après avoir atteint l'exp requis
+	 * Niveau + 1 et augmentation des dégats
+	 */
+	
+	public void niveauSuperieur() {
+		int degat = this.getDegat();
+		this.setNiveau(this.getNiveau() + 1);
+		this.setDegat(degat + ((this.getNiveau() / 10) * degat));
+	}
+	
+}
