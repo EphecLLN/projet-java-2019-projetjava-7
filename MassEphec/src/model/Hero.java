0db2dcc23cee607931171ec3c5fec37a5a446e4b
he201749 <he201749@students.ephec.be>
2019-11-15 12:55:36 +0100

    ajout hero et test hero

---
+++ MassEphec/src/model/Hero.java
@@ -0,0 +1,75 @@
+package model;
+public class Hero extends Personnage{
+	
+	private int credit; 
+	//private Arme arme;
+	private String classe;// IT compta ou market
+	
+	public Hero(String nom, int vie, int x, int y, String classe) {//+Arme arme
+		super(nom,vie,x,y);
+		this.credit=0;
+		//this.arme=arme;
+		this.classe=classe;
+	}
+	
+	/*
+	 * Fournit le nombre de credits que l'etudiant possede
+	 * @return {int} le nombre de credits
+	 */
+	public int getCredit() {
+		return this.credit;
+	}
+	/*
+	 * Modifie le nombre de credits que l'etudiant possede
+	 * @param {int} le nombre de credits à ajouter
+	 */
+	public void setCredit(int creds) {
+		this.credit=creds;
+	}
+	/*
+	 * Fournit la classe du hero
+	 * @return {String} sa classe
+	 */
+	public String getClasse() {
+		return this.classe;
+	}
+	/*
+	 * Fait bouger l'etudiant d'une case vers la droite
+	 */
+	public void bougerDroite() {
+		if(this.getCoordX()<15) {
+			this.setCoordX(this.getCoordX()+1);
+		}
+	}
+	/*
+	 * Fait bouger l'etudiant d'une case vers la gauche
+	 */
+	public void bougerGauche() {
+		if(this.getCoordX()>0) {
+			this.setCoordX(this.getCoordX()-1);
+		}
+	}
+	/*
+	 * Fait bouger l'etudiant d'une case vers le haut
+	 */
+	public void bougerHaut() {
+		if(this.getCoordY()>0) {
+			this.setCoordY(this.getCoordY()-1);
+		}
+	}
+	/*
+	 * Fait bouger l'etudiant d'une case vers le bas
+	 */
+	public void bougerBas() {
+		if(this.getCoordY()<15) {
+			this.setCoordY(this.getCoordY()+1);
+		}
+	}
+	/*
+	 * attaque un monstre en lui enlevant de la vie;
+	 * @param{Monstre} le monstre a attaquer
+	 */
+	/*public void attaque(Monstre m) {
+		this.arme.attaqueDeBase(m);
+	}*/
+}
---
+++ MassEphec/src/model/HeroTest.java
@@ -0,0 +1,57 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HeroTest {
+
+	@Test
+	void testConstructeur() {//test constructeur avec maxime et IT
+		Hero nv = new Hero("maxime",100,0,0,"IT");
+		assertEquals(0,nv.getCredit());
+		assertEquals("IT",nv.getClasse());
+	}
+	@Test
+	void testSetCredit() {//test de setCredit en ajoutant 60 credits
+		Hero nv = new Hero("maxime",100,0,0,"IT");
+		nv.setCredit(60);
+		assertEquals(60,nv.getCredit());
+	}
+	@Test
+	void testBougerDroite() {//test de la methode bougerDroite, on test lorsque la coord x vaut 0 et vaut 15(cas critique)
+		Hero nv = new Hero("maxime",100,0,0,"IT");
+		nv.bougerDroite();
+		assertEquals(1,nv.getCoordX());
+		nv.setCoordX(15);
+		nv.bougerDroite();
+		assertEquals(15,nv.getCoordX());
+	}
+	@Test
+	void testBougerGauche() {//test de la methode bougerGauche, on test lorsque la coord x vaut 0(cas critique) et vaut 15
+		Hero nv = new Hero("maxime",100,0,0,"IT");
+		nv.bougerGauche();
+		assertEquals(0,nv.getCoordX());
+		nv.setCoordX(15);
+		nv.bougerGauche();
+		assertEquals(14,nv.getCoordX());
+	}
+	@Test
+	void testBougerHaut() {//test de la methode bougerHaut, on test lorsque la coord Y vaut 0(cas critique) et vaut 15
+		Hero nv = new Hero("maxime",100,0,0,"IT");
+		nv.bougerHaut();
+		assertEquals(0,nv.getCoordY());
+		nv.setCoordY(15);
+		nv.bougerHaut();
+		assertEquals(14,nv.getCoordY());
+	}
+	@Test
+	void testBougerBas() {//test de la methode bougerBas, on test lorsque la coord Y vaut 0 et vaut 15(cas critique)
+		Hero nv = new Hero("maxime",100,0,0,"IT");
+		nv.bougerBas();
+		assertEquals(1,nv.getCoordY());
+		nv.setCoordY(15);
+		nv.bougerBas();
+		assertEquals(15,nv.getCoordY());
+	}
+}
