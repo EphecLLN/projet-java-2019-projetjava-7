5530033746de9c39cf8de62089abbe5c5ea8d52c
Emmanuel <e.foureau@students.ephec.be>
2019-11-22 09:41:31 +0100

    Ajout de la classe Pc

---
+++ MassEphec/src/model/Pc.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author emman
+ *
+ */
+public class Pc extends Arme {
+	
+	public Pc () {
+		super("Pc", 0, 20);
+	}
+	
+	public void attaqueGit(Monstre ennemi) { 
+		int totalDegat = (int) (this.getDegat() * 2);
+		ennemi.setVie(ennemi.getVie() - totalDegat);
+	}
+}
