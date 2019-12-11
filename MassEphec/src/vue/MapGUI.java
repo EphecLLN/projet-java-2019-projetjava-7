147815fe9af7d1bdcc4d65375e3be18be09bac2c
EmmanuelF-Ephec <56127389+EmmanuelF-Ephec@users.noreply.github.com>
2019-12-11 12:17:48 +0100

    Methode gdx delete

--- MassEphec/src/vue/MapGUI.java
+++ MassEphec/src/vue/MapGUI.java
@@ -35,46 +35,4 @@ public class MapGUI implements Observer {
 	public void update(Observable o, Object arg) {
 		
 	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void render(float delta) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void resize(int width, int heigth) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-		
-	}
 }
